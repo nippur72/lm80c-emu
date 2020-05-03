f8e7ddd6545bc09a42bc34c5856acfa2799ea5f5
nino-porcino <nino.porcino@gmail.com>
2020-05-03 17:43:17 +0200

    add screen2_putc routine

---
+++ software/screen2_putc/m.bat
@@ -0,0 +1,1 @@
+yaza screen2_putc.asm --output:screen2_putc.prg --lst --sym
---
+++ software/screen2_putc/screen2_putc.asm
@@ -0,0 +1,95 @@
+; firmware v3.8
+VDP_DAT    equ $0030   ; VDP data port
+CHRST88    equ $427B   ; start of 8x8 font in firmware ROM
+SETVDPADRS equ $05E0   ; firmware routine that sets VDP address in HL
+
+org $A000
+
+;
+; Prints a character in screen 2 mode
+;
+; formula is:
+; SOURCE_ADDRESS = CHRST88 + character*8
+; DEST_BMP_ADDRESS = X*8 + Y*256
+; DEST_COLOR_ADDRES = &H2000 + X*8 + Y*256
+;
+
+CH:  db 0   ; B = character
+COL: db 0   ; C = color (fg+bg)
+X:   db 0   ; D = x coordinate (column 0-31)
+Y:   db 0   ; E = y coordinate (row 0-23)
+
+SCREEN2_PUTC:
+   PUSH BC
+   PUSH DE
+   PUSH HL
+
+   ; calculate source address
+   LD    A, (CH)      ;
+   LD    L, A         ;
+   LD    H, 0         ;
+   ADD   HL, HL       ;
+   ADD   HL, HL       ;
+   ADD   HL, HL       ; HL = ch * 8
+   LD    DE, CHRST88  ; DE = start of 8x8 fonts in ROM
+   ADD   HL, DE       ; HL = start of character in ROM
+
+   PUSH  HL           ; save calculated address
+
+   ; calculate dest address in bitmap vram
+   LD    A, (X)       ;
+   LD    L, A         ;
+   LD    H, 0         ; HL = X
+   ADD   HL, HL       ;
+   ADD   HL, HL       ;
+   ADD   HL, HL       ; HL = X*8
+   LD    A, (Y)
+   LD    D, A         ;
+   LD    E, 0         ; DE = Y * 256
+   ADD   HL, DE       ; HL = X*8 + Y*256
+
+   PUSH  HL           ; save dest
+
+   ; calculate dest address in color vram
+   LD    DE, $2000     ;
+   ADD   HL, DE        ; HL = $2000 + X*8 + Y*256
+
+   DI
+;----------------------
+
+fill_col:
+   ; HL contains the address
+   CALL  SETVDPADRS
+   LD    B, 8            ; repeat for 8 rows
+   LD    C, VDP_DAT      ; VDP data mode
+   LD    A, (col)
+fill_col_1:
+   OUT   (C), A
+   NOP
+   NOP
+   NOP
+   DJNZ  fill_col_1
+
+copia_bmp:
+   POP   HL              ; dest address was saved in stack
+   CALL  SETVDPADRS
+   POP   HL              ; source address was saved in stack
+   LD    B, 8            ; repeat for 8 rows
+   LD    C, VDP_DAT      ; VDP data mode
+
+copia_bmp_1:
+   OUTI
+   NOP                   ; wait...
+   NOP                   ; ...a while
+   NOP                   ;
+   JR   NZ, copia_bmp_1
+
+;----------------------
+
+   EI
+
+   POP HL
+   POP DE
+   POP BC
+   RET
+
