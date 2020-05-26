01a3e852a19c0309aa85e6bd9ec41be15ba5304a
nino-porcino <nino.porcino@gmail.com>
2020-05-26 09:41:13 +0200

    clean up audio code

--- emulator.js
+++ emulator.js
@@ -1,7 +1,8 @@
 "use strict";
 
+// TODO fix AY-3-8910
+// TODO keyboard buffering
 // TODO check actual timings (elapsed)
-// TODO list of 8 bit fonts? COLECO, MSX, APPLE/LASER, TOPAZ, AMSTRAD, CGA, PET/VIC, C64
 // TODO rename paste into serial
 // TODO serial output buffer for printing
 // TODO investigate why dropping "screen2_putc.prg" hangs it
@@ -9,8 +10,10 @@
 // TODO implement SIO-CTC-PIO daisy chain
 // TODO fix autoload
 // TODO tms timings check 30 T states
+// TODO list of 8 bit fonts? COLECO, MSX, APPLE/LASER, TOPAZ, AMSTRAD, CGA, PET/VIC, C64
 
-// firmware 3.11
+
+// firmware 3.12
 let BASTXT     = 0x8133;
 let PROGND     = 0x81BB;
 let CRSR_STATE = 0x81E9;
@@ -80,12 +83,6 @@ function renderLines(nlines) {
          if(debugAfter !== undefined) debugAfter(elapsed);
          cycle += elapsed;         
          total_cycles += elapsed;
-
-         //if(sample !== 0) console.log("hhh");
-         //writeAudioSamples(elapsed, sample);
-
-         //cloadAudioSamples(elapsed);
-         //if(csaving) csaveAudioSamples(elapsed);
 
          if(ctc_enabled)
          {
@@ -174,33 +171,6 @@ function ay38910_audio_buf_ready(ptr, size) {
 
    //console.log(`${elapsed} arrived: ${audio_buffers_queue.length}`);
    audio_buffers_queue.push(buffer);
-
-   for(let i=0; i<buffer.length; i++) {   
-      if(csaveBuffer !== undefined) {
-         csaveBuffer[csavePtr++] = buffer[i];
-      }         
-   }
-}
-
-// ********************************* CPU TO AUDIO BUFFER *********************************************
-
-const audioBufferSize = 4096; // enough to hold more than one frame time
-const audioBuffer = new Float32Array(audioBufferSize);
-
-let audioPtr = 0;                // points to the write position in the audio buffer (modulus)
-let audioPtr_unclipped = 0;      // audio buffer writing absolute counter 
-let downSampleCounter = 0;       // counter used to downsample from CPU speed to 48 Khz
-
-function writeAudioSamples(n, s) {
-   downSampleCounter += (n * sampleRate);
-   if(downSampleCounter >= cpuSpeed) {
-      //let s = (speaker_A ? -0.5 : 0.0);
-      //if(tape_monitor) s += (cassette_bit_out ? 0.5 : 0.0) + (cassette_bit_in ? 0.0 : 0.5);
-      downSampleCounter -= cpuSpeed;
-      audioBuffer[audioPtr++] = s;
-      audioPtr = audioPtr % audioBufferSize;
-      audioPtr_unclipped++;
-   }      
 }
 
 // ********************************* AUDIO BUFFER TO BROWSER AUDIO ************************************
@@ -209,9 +179,6 @@ let audioContext = new (window.AudioContext || window.webkitAudioContext)();
 const bufferSize = 4096;
 const sampleRate = audioContext.sampleRate;
 var speakerSound = audioContext.createScriptProcessor(bufferSize, 1, 1);
-
-let audioPlayPtr = 0;
-let audioPlayPtr_unclipped = 0;
 
 let last_browser_call = 0;
 let last_browser_call_msec = 0;
@@ -228,8 +195,6 @@ speakerSound.onaudioprocess = function(e) {
       console.log("warning: audio queue is empty");
       for(let i=0; i<bufferSize; i++) {         
          output[i] = audioBuffer[i];
-         if(csaveBuffer !== undefined)
-            csaveBuffer[csavePtr++] = audioBuffer[i];
        }
       return;
    }
@@ -259,10 +224,6 @@ speakerSound.onaudioprocess = function(e) {
    // playback what is in the audio buffer
    for(let i=0; i<bufferSize; i++) {   
       output[i] = buffer[i];       
-      /*
-      if(csaveBuffer !== undefined)
-         csaveBuffer[csavePtr++] = buffer[i];
-      */
     }
 }
 
@@ -292,12 +253,6 @@ speakerSound.onaudioprocessx = function(e) {
 let audio_playing = false;
 
 function goAudio() {
-   audioPlayPtr_unclipped = 0;
-   audioPlayPtr = 0;
-
-   audioPtr = 0;
-   audioPtr_unclipped = 0;
-
    speakerSound.connect(audioContext.destination);
    audio_playing = true;
 }
@@ -338,62 +293,6 @@ function cloadAudioSamples(n) {
       tapePtr++;      
    }
 }
-
-// ********************************* CPU TO CSAVE BUFFER *********************************************
-
-const csaveBufferSize = 44100 * 5 * 60; // five minutes max
-
-let csaveBuffer;                 // holds the tape audio for generating the WAV file
-let csavePtr;                    // points to the write position in the csaveo buffer 
-let csaveDownSampleCounter;      // counter used to downsample from CPU speed to 48 Khz
-
-let csaving = false;
-
-function csaveAudioSamples(n) {
-   /*
-   csaveDownSampleCounter += (n * 44100);
-   if(csaveDownSampleCounter >= cpuSpeed) {
-      const s = (cassette_bit_out ? 0.75 : -0.75);
-      csaveDownSampleCounter -= cpuSpeed;
-      csaveBuffer[csavePtr++] = s;
-   }
-   */
-}
-
-function csave() {
-   csavePtr = 0;
-   csaveDownSampleCounter = 0;
-   csaveBuffer = new Float32Array(csaveBufferSize);
-   csaving = true;
-   console.log("saving audio (max 5 minutes); use cstop() to stop recording");
-}
-
-function cstop() {
-   csaving = false;
-
-   // trim silence before and after
-   //const start = csaveBuffer.indexOf(0.75);
-   //const end = csaveBuffer.lastIndexOf(0.75);
-   const start = 1;
-   const end = csaveBuffer.length;
-
-   const audio = csaveBuffer.slice(start, end);
-   const length = Math.round(audio.length / 44100);
-   
-   const wavData = {
-      sampleRate: 44100,
-      channelData: [ audio ]
-   };
-     
-   const buffer = encodeSync(wavData, { bitDepth: 16, float: false });      
-   
-   let blob = new Blob([buffer], {type: "application/octet-stream"});   
-   const fileName = "csaved.wav";
-   saveAs(blob, fileName);
-   console.log(`downloaded "${fileName}" (${length} seconds of audio)`);
-}
-
-/*********************************************************************************** */
 
 function main() {
    // prints welcome message on the console
