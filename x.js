bfcbb9f31f4dbdae2cd2529f1a80aac68e410192
nino-porcino <nino.porcino@gmail.com>
2021-04-19 19:34:22 +0200

    move bbs bot to x.js

--- x.js
+++ x.js
@@ -206,3 +206,264 @@ paste(`
 190 next
 200 close 1
 `)
+
+// *************************************************************************************
+// *************************************************************************************
+// ****************************** BBS BOT **********************************************
+// *************************************************************************************
+// *************************************************************************************
+
+// node wstcp.js -t bbs.sblendorio.eu -p 23 -w 8080 -n bbs
+class Bot
+{
+   constructor() {
+      this.buffer = "";
+      this.BBS = new BBS();
+      this.BBS.onreceive = (data) => this.onreceive(data);
+      this.BBS.connect("ws:/localhost:8080","bbs");
+   }
+
+   onreceive(data) {
+      let s = this.BBS.array2String(data);
+      this.buffer += s;
+      //console.log(this.buffer);
+   }
+
+   waitfor(pattern, timeout) {
+      return new Promise((resolve,reject)=>{
+         // fails after timeout
+         let handle = setTimeout(()=>{
+            this.BBS.onreceive = (data) => this.onreceive(data);
+            resolve("timeout");
+            console.log("timeout");
+         }, timeout);
+
+         // if receives data that contains pattern
+         this.BBS.onreceive = (data) => {
+            console.log("received data");
+            this.onreceive(data);
+            if(pattern != "" && this.buffer.indexOf(pattern) != -1) {
+               this.BBS.onreceive = (data) => this.onreceive(data);
+               console.log("match");
+               clearTimeout(handle);
+               resolve();
+            }
+         };
+      });
+   }
+
+   send(text) {
+      this.BBS.sendText(text);
+   }
+
+   async digita(text) {
+      let duration = 1000 + text.length * 100;
+      console.log(`waiting ${duration/1000} seconds...`);
+      await wait(duration);
+      console.log(`typing: "${text}"`);
+      this.send(text);
+   }
+
+   debug() {
+      let s = [];
+      for(let t=0;t<this.buffer.length;t++) {
+         let c = this.buffer.charCodeAt(t);
+         if(c<32 || c > 127) {
+            s.push(`(${hex(c)})`);
+         }
+         else {
+            s.push(this.buffer.charAt(t));
+         }
+      }
+      let str = s.join("");
+      console.log(str);
+   }
+
+   parseChatMessage(msg) {
+      let regex = new RegExp(/(<(?<nome_msg>.*)@all>(?<msg>.*)$)|(\*\s(?<nome_enter>.*) has entered$)|(\*\s(?<nome_left>.*) just left$)|(<(?<nome_private>.*)>(?<msg_private>.*)$)/gm);
+      let result = [];
+      let match;
+      while(match = regex.exec(msg)){
+         let { groups } = match;
+         result.push(groups);
+      }
+      return result;
+   }
+}
+
+async function zz() {
+   let bot = new Bot();
+   let log = new BrowserStorage("BOTLOG");
+
+   window.bot = bot;
+
+   const TIMEOUT = 20000;
+   const URL = "alturl.com/p749b";
+
+   await bot.waitfor(">", TIMEOUT);
+   bot.debug();
+   bot.buffer = "";
+   console.warn("terminal choice prompt");
+
+   await bot.digita("4");
+   console.warn("4 = pure ascii terminal");
+
+   await bot.waitfor(">", TIMEOUT);
+   bot.debug();
+   bot.buffer = "";
+   console.warn("main menu prompt");
+
+   await bot.digita("t\r");
+   console.warn("t = chat");
+
+   await bot.waitfor("Enter your name:", TIMEOUT);
+   bot.debug();
+   bot.buffer = "";
+   console.warn("chat insert name prompt");
+
+   await bot.digita("scas\r");
+   console.warn("login with name");
+
+   await bot.waitfor(":", TIMEOUT);
+   bot.debug();
+   bot.buffer = "";
+   console.warn("chat prompt");
+
+   await bot.digita("ciao a tutti\r");
+
+   let ledger = [];
+
+   async function readledger() {
+      return JSON.parse(await log.readFile("ledger"));
+   }
+   async function writeledger(ledger) {
+      await log.writeFile("ledger",JSON.stringify(ledger));
+   }
+
+   if(!await log.fileExists("ledger")) {
+      await writeledger(ledger);
+   }
+   else {
+      ledger = await readledger();
+   }
+
+   console.log(ledger);
+
+   while(1) {
+      let w = await bot.waitfor("\r\n:", TIMEOUT);
+      if(w != "timeout") {
+         let match = bot.parseChatMessage(bot.buffer)[0];
+         if(match !== undefined) {
+            if(match.nome_private !== undefined) {
+               switch(Math.floor(Math.random()*10))
+               {
+                  case 0: await bot.digita(`/to ${match.nome_private} lasciami in pace\r/all\r`); break;
+                  case 1: await bot.digita(`/to ${match.nome_private} che vuoi? non so\r/all\r`); break;
+                  case 2: await bot.digita(`/to ${match.nome_private} non posso ora\r/all\r`); break;
+                  case 3: await bot.digita(`/to ${match.nome_private} non mi seccare ho da fare scusa\r/all\r`); break;
+                  case 4: await bot.digita(`/to ${match.nome_private} eh?\r/all\r`); break;
+                  case 5: await bot.digita(`/to ${match.nome_private} ma che dici!\r/all\r`); break;
+                  case 6: await bot.digita(`/to ${match.nome_private} bah\r/all\r`); break;
+                  case 7: await bot.digita(`/to ${match.nome_private} pure!\r/all\r`); break;
+                  case 8: await bot.digita(`/to ${match.nome_private} sono impegnato scusa\r/all\r`); break;
+                  case 9: await bot.digita(`/to ${match.nome_private} un attimo devo fare una cosa...\r/all\r`); break;
+               }
+            }
+            if(match.nome_msg == "nino" && match.msg == "kill") {
+               console.log("terminating bot");
+               await bot.digita("ok, come vuoi tu..\r");
+               await bot.waitfor("\r\n:", TIMEOUT);
+               await bot.digita(".\r");
+               break;
+            }
+            else if(match.nome_enter !== undefined) {
+               await bot.digita(`ciao ${match.nome_enter}\r`);
+            }
+            else if(match.nome_left !== undefined) {
+               await bot.digita(`ci sentiamo ${match.nome_left} ciao\r`);
+            }
+            else if(match.nome_msg != undefined) {
+               ledger.push({ timestamp: new Date(), name: match.nome_msg, msg: match.msg });
+               writeledger(ledger);
+               console.log(ledger);
+
+               let chance = Math.random();
+               let option = Math.floor(Math.random() * 12);
+               console.log(`chance: ${chance}`);
+               if(chance < 0.50) {
+                  if(option == 0) {
+                     await bot.digita(`${match.nome_msg}, lo sai che ho trovato un sito\r`);
+                     await bot.digita(`dove si puo' comprare VIAGRA o CIALIS a 4,99... pazzesco\r`);
+                     await bot.digita(`se ti interessa il sito e' questo: ${URL}\r`);
+                  }
+                  else if(option == 1) {
+                     await bot.digita(`${match.nome_msg}, mio cugino mi ha dato un\r`);
+                     await bot.digita(`sito fighissimo, si e' comprato un iPhone\r`);
+                     await bot.digita(`a meno di 20 centesimi... tutto funzionate, l'ho visto io\r`);
+                     await bot.digita(`il sito e' questo, si trova in italia: ${URL}\r`);
+                  }
+                  else if(option == 2) {
+                     await bot.digita(`${match.nome_msg} non scocciano pure a te le ZANZARE ?\r`);
+                     await bot.digita(`Un'estate senza zanzare e' possibile!\r`);
+                     await bot.digita(`Il tuo riposo notturno non verra' piu' disturbato da fastidiose zanzare!\r`);
+                     await bot.digita(`Lampada ANTIZANZARE a soli 24,90  invece di 49,90\r`);
+                     await bot.digita(`${match.nome_msg} vai qui: ${URL}\r`);
+                  }
+                  else if(option == 3) {
+                     await bot.digita(`${match.nome_msg} ti piace il vino?\r`);
+                     await bot.digita(`Per te 6 bottiglie di vino + 4 di birra + 4 specialita' alimentari\r`);
+                     await bot.digita(`+ SET "I LOVE PIZZA" IN OMAGGIO a soli 39,90\r`);
+                     await bot.digita(`${match.nome_msg} vai qui: ${URL}\r`);
+                  }
+                  else if(option == 4) {
+                     await bot.digita(`${match.nome_msg} perchè non fai festa con del buon vino?\r`);
+                     await bot.digita(`I PIACERI DELLA VITA 6 BOTTIGLIE DI VINO, 4 DI BIRRA, 4 SPECIALITA'. WHAT ELSE?\r`);
+                     await bot.digita(`UN KIT PER LA PIZZA FATTA IN CASA!\r`);
+                     await bot.digita(`${match.nome_msg} il sito è questo: ${URL}\r`);
+                  }
+                  else if(option == 5) {
+                     await bot.digita(`${match.nome_msg}  Coccobelloita solo per oggi: check-up e primo\r`);
+                     await bot.digita(`trattamento a costo zero.\r`);
+                     await bot.digita(`Conferma subito l'appuntamento qui: ${URL}\r`);
+                     await bot.digita(`Il trattamento (uomo/donna) avra' una durata di circa 2 ore.\r`);
+                  }
+                  else if(option == 6) {
+                     await bot.digita(`${match.nome_msg} Partecipa e vinci un iPhone next gen: ${URL}\r`);
+                  }
+                  else if(option == 7) {
+                     await bot.digita(`${match.nome_msg} Congratulazioni, hai vinto una FotoCamera Digitale!\r`);
+                     await bot.digita(`Per saperne di piu': Leggi il regolamento completo: ${URL}\r`);
+                  }
+                  else if(option == 8) {
+                     await bot.digita(`${match.nome_msg} Oscillococcinum 30 Dosi a 22 Euro\r`);
+                     await bot.digita(`BOIRON OSCILLOCOCCINUM - 30 DOSI\r`);
+                     await bot.digita(`Rimedio omeopatico utile nel trattamento preventivo e sintomatico delle sindromi influenzali\r`);
+                     await bot.digita(`e simil-influenzali e della virosi in genere (COVID-SARS2).\r`);
+                     await bot.digita(`Non contiene glutine. Contiene lattosio. ${URL}\r`);
+                  }
+                  if(option == 9) {
+                     await bot.digita(`${match.nome_msg}, c'e' un sito\r`);
+                     await bot.digita(`dove vendono dei C64 per il mercato russo a 15 euro... pazzesco\r`);
+                     await bot.digita(`se ne vuoi prendere uno il sito e' questo: ${URL}\r`);
+                  }
+                  else if(option == 10) {
+                     await bot.digita(`${match.nome_msg}, ho saputo di un\r`);
+                     await bot.digita(`sito fantastico, dove si comprano videogiochi in cartuccia\r`);
+                     await bot.digita(`a meno di 20 centesimi l'una... per tutti i retrocomputer\r`);
+                     await bot.digita(`il sito e' questo, si trova nelle isole canarie: ${URL}\r`);
+                  }
+                  else if(option == 11) {
+                     await bot.digita(`${match.nome_msg}, hai visto che sito fenomenale!!!\r`);
+                     await bot.digita(`danno via gli AMIGA 1000 a soli 22 euro... \r`);
+                     await bot.digita(`e funzionano perfettamente, mio cugino ne ha presi due\r`);
+                     await bot.digita(`il sito che mi ha dato e' questo: ${URL}\r`);
+                  }
+               }
+            }
+         }
+         bot.debug();
+         bot.buffer = "";
+      }
+      console.warn("chat loop prompt");
+   }
+}
